0a17f913dba540c7b709929e1ae7960a6deb411d
FULLSTACK <jumin0220@naver.com>
2023-06-09 12:00:28 +0900

    프로젝트 저장소 초기화

---
+++ src/main/java/jiho/hello/cicd/jenkinscicd1/JenkinsCiCd1Application.java
@@ -0,0 +1,13 @@
+package jiho.hello.cicd.jenkinscicd1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCiCd1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCiCd1Application.class, args);
+    }
+
+}
---
+++ src/test/java/jiho/hello/cicd/jenkinscicd1/JenkinsCiCd1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jiho.hello.cicd.jenkinscicd1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCiCd1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
