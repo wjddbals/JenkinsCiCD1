0af19c8bbf9a4ce51307967008b57359871937aa
FULLSTACK <jumin0220@naver.com>
2023-06-13 12:12:47 +0900

    port번호 변경

--- src/main/java/jiho/hello/cicd/jenkinscicd1/JenkinsCiCd1Application.java
+++ src/main/java/jiho/hello/cicd/jenkinscicd1/JenkinsCiCd1Application.java
@@ -2,7 +2,7 @@ package jiho.hello.cicd.jenkinscicd1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//http://15.164.96.98:9080/
 @SpringBootApplication
 public class JenkinsCiCd1Application {
 
